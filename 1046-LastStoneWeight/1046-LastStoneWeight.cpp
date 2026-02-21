631d900f357b2a0af30fad8dae34a5a6659145a8
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-02-21 15:55:47 +0700

    File updated at 2/21/2026, 3:55:47 PM

--- 1046-LastStoneWeight/1046-LastStoneWeight.cpp
+++ 1046-LastStoneWeight/1046-LastStoneWeight.cpp
@@ -1,33 +1,41 @@
-// Last updated: 2/20/2026, 9:49:13 PM
-1struct Node{
-2    int sum;
-3    int i;
-4    int j;
-5    bool operator>(const Node& other)const{
-6        return sum > other.sum;
-7    }
-8};
-9class Solution {
-10    
-11public:
-12    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
-13        vector<vector<int>> res;
-14        int n=nums1.size();
-15        int m=nums2.size();
-16        if (m==0||n==0||k==0) return res;
-17        priority_queue<Node, vector<Node>, greater<Node>> pq;
-18        for (int i=0;i<n;++i){
-19            pq.push({nums1[i]+nums2[0], i, 0});
-20        }
-21        while (k>0 && !pq.empty()){
-22            Node curr = pq.top();
-23            pq.pop();
-24            res.push_back({nums1[curr.i], nums2[curr.j]});
-25            k--;
-26            if (curr.j+1<m){
-27                pq.push({nums1[curr.i] + nums2[curr.j + 1], curr.i, curr.j + 1});
-28            }
-29        }
-30        return res;
-31    }
-32};
+// Last updated: 2/21/2026, 3:55:47 PM
+1#include <vector>
+2#include <queue>
+3
+4using namespace std;
+5
+6class Solution {
+7public:
+8    bool isPossible(vector<int>& target) {
+9        priority_queue<long long> pq;
+10        long long total_sum = 0;
+11        for (int num : target) {
+12            pq.push(num);
+13            total_sum += num;
+14        }
+15
+16        while (true) {
+17            long long max_val = pq.top();
+18            pq.pop();
+19            if (max_val == 1) {
+20                return true;
+21            }
+22            long long rest_sum = total_sum - max_val;
+23            if (rest_sum == 1) {
+24                return true;
+25            }
+26            // Các trường hợp không hợp lệ:
+27            // 1. rest_sum == 0: Mảng chỉ có 1 phần tử > 1
+28            // 2. max_val <= rest_sum: Giá trị gốc sẽ <= 0
+29            if (rest_sum == 0 || max_val <= rest_sum) {
+30                return false;
+31            }
+32            long long prev_val = max_val % rest_sum;
+33            if (prev_val == 0) {
+34                return false;
+35            }
+36            total_sum = rest_sum + prev_val;
+37            pq.push(prev_val);
+38        }
+39    }
+40};

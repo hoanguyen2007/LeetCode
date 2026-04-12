39fad36b2aec056151b75e9f1a3bf187ae49aa1e
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-04-12 12:02:01 +0700

    File updated at 4/12/2026, 12:02:00 PM

--- 831-MaskingPersonalInformation/831-MaskingPersonalInformation.cpp
+++ 831-MaskingPersonalInformation/831-MaskingPersonalInformation.cpp
@@ -1,27 +1,8 @@
-// Last updated: 4/12/2026, 11:17:56 AM
+// Last updated: 4/12/2026, 12:02:00 PM
 1class Solution {
 2public:
-3   string maskPII(string s) {
-4    if (isalpha(s[0])) { 
-5        int at_pos = s.find('@');
-6        string res = "";
-7        res += tolower(s[0]);
-8        res += "*****";
-9        res += tolower(s[at_pos - 1]);
-10        res += "@";
-11        for (int i = at_pos + 1; i < s.length(); ++i) {
-12            res += tolower(s[i]);
-13        }
-14        return res;
-15    } else {
-16        string d = "";
-17        for (char c : s) if (isdigit(c)) d += c;
-18        
-19        string local = "***-***-" + d.substr(d.length() - 4);
-20        int country = d.length() - 10;
-21        
-22        if (country == 0) return local;
-23        return "+" + string(country, '*') + "-" + local;
-24    }
-25}
-26};
+3    bool repeatedSubstringPattern(string s) {
+4    string doubled = s + s;
+5    return doubled.substr(1, doubled.length() - 2).find(s) != string::npos;
+6}
+7};

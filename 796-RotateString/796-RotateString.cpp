36915befba1a86e4eeb09f078384a245f4e99461
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-04-18 14:54:30 +0700

    File updated at 4/18/2026, 2:54:30 PM

--- 796-RotateString/796-RotateString.cpp
+++ 796-RotateString/796-RotateString.cpp
@@ -1,14 +1,26 @@
-// Last updated: 4/18/2026, 2:33:29 PM
+// Last updated: 4/18/2026, 2:54:30 PM
 1class Solution {
 2public:
-3    bool rotateString(string s, string goal) {
-4        int initial = goal.length();
-5        goal+= goal;
-6        if (initial==s.length()){
-7        if (goal.find(s)==string::npos || goal.find(s)>initial){
-8            return false;
-9        }
-10        return true;}
-11        else return false;
-12    }
-13};
+3    int repeatedStringMatch(string a, string b) {
+4        string source = "";
+5        int count = 0;
+6        
+7        while (source.length() < b.length()) {
+8            source += a;
+9            count++;
+10        }
+11        
+12        if (source.find(b) != string::npos) {
+13            return count;
+14        }
+15        
+16        source += a;
+17        count++;
+18        
+19        if (source.find(b) != string::npos) {
+20            return count;
+21        }
+22        
+23        return -1;
+24    }
+25};

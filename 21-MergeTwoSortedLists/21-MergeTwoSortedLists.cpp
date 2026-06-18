607c7a79fa9ca74c207e7dbfc21c42c3455a5995
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-06-18 16:35:39 +0700

    File updated at 6/18/2026, 4:35:37 PM

--- 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -1,21 +1,22 @@
-// Last updated: 6/18/2026, 3:59:25 PM
+// Last updated: 6/18/2026, 4:35:37 PM
 1class Solution {
 2public:
-3    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-4        ListNode dummy(0); 
-5        ListNode* tail = &dummy;
-6        
-7        while (list1 != nullptr && list2 != nullptr) {
-8            if (list1->val <= list2->val) {
-9                tail->next = list1; 
-10                list1 = list1->next; 
-11            } else {
-12                tail->next = list2; 
-13                list2 = list2->next; 
-14            }
-15            tail = tail->next;       
-16        }
-17        tail->next = (list1 != nullptr) ? list1 : list2;
-18        return dummy.next;
-19    }
-20};
+3    ListNode* swapPairs(ListNode* head) {
+4        ListNode dummy(0);
+5        dummy.next = head;
+6        ListNode* prev = &dummy;
+7        while (prev->next != nullptr && prev->next->next != nullptr) {
+8            ListNode* node1 = prev->next;
+9            ListNode* node2 = prev->next->next;
+10            
+11            prev->next = node2;
+12            
+13            node1->next = node2->next; 
+14            node2->next = node1;       
+15            
+16            prev = node1; 
+17        }
+18        
+19        return dummy.next;
+20    }
+21};

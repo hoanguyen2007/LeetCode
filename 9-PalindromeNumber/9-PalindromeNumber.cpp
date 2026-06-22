afce025015701022af15ab2e7b0f234a405506ad
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-06-22 16:28:19 +0700

    File updated at 6/22/2026, 4:28:19 PM

--- 9-PalindromeNumber/9-PalindromeNumber.cpp
+++ 9-PalindromeNumber/9-PalindromeNumber.cpp
@@ -1,17 +1,21 @@
-// Last updated: 6/22/2026, 4:26:52 PM
-1class Solution {
-2public:
-3    bool isPalindrome(int x) {
-4        if (x < 0 || (x % 10 == 0 && x != 0)) {
-5            return false;
-6        }
-7
-8        int reversedHalf = 0;
-9
-10        while (x > reversedHalf) {
-11            reversedHalf = reversedHalf * 10 + x % 10;
-12            x /= 10;
-13        }
-14        return x == reversedHalf || x == reversedHalf / 10;
-15    }
-16};
+// Last updated: 6/22/2026, 4:28:19 PM
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0
+        // (except 0 itself) cannot be palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == reversedHalf
+        // For odd digits: x == reversedHalf / 10
+        return x == reversedHalf || x == reversedHalf / 10;
+    }
+};

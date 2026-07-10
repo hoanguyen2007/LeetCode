2a6821ca7304bb2ec1832939feef525670c58a74
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-07-10 12:09:04 +0700

    File updated at 7/10/2026, 12:09:03 PM

--- 141-LinkedListCycle/141-LinkedListCycle.cpp
+++ 141-LinkedListCycle/141-LinkedListCycle.cpp
@@ -1,30 +1,29 @@
-// Last updated: 7/10/2026, 12:08:37 PM
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode(int x) : val(x), next(NULL) {}
-7 * };
-8 */
-9class Solution {
-10public:
-11    bool hasCycle(ListNode *head) {
-12        if (head == nullptr || head->next == nullptr) {
-13            return false;
-14        }
-15
-16        ListNode *slow = head;
-17        ListNode *fast = head;
-18
-19        while (fast != nullptr && fast->next != nullptr) {
-20            slow = slow->next;         
-21            fast = fast->next->next; 
-22            if (slow == fast) {
-23                return true; 
-24            }
-25        }
-26
-27        return false; 
-28    }
-29};
+// Last updated: 7/10/2026, 12:09:03 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return false;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast != NULL && fast->next !=NULL){
+
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                return true;
+            }
+
+           
+        }
+        return false;
+
+    }
+};

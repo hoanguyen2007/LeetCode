ec1540f4ae6e6389d8f94d1d7cfd4bdfddbfa999
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-07-15 21:41:43 +0700

    File updated at 7/15/2026, 9:41:44 PM

--- 142-LinkedListCycleII/142-LinkedListCycleII.cpp
+++ 142-LinkedListCycleII/142-LinkedListCycleII.cpp
@@ -1,26 +1,29 @@
-// Last updated: 7/15/2026, 9:40:55 PM
-1class Solution {
-2public:
-3    ListNode *detectCycle(ListNode *head) {
-4        if(head == NULL || head->next == NULL) return nullptr;
-5        
-6        ListNode* slow = head;
-7        ListNode* fast = head;
-8        
-9        while(fast != NULL && fast->next != NULL){
-10            slow = slow->next;
-11            fast = fast->next->next;
-12            
-13            if(slow == fast){
-14                ListNode* entry = head; 
-15                   while(entry != slow) {
-16                    entry = entry->next;
-17                    slow = slow->next;
-18                }
-19                
-20                return entry; 
-21                }
-22        }
-23        return nullptr; 
-24    }
-25};
+// Last updated: 7/15/2026, 9:41:44 PM
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast!=nullptr && fast->next!=nullptr){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow){
+                ListNode* ptr=head;
+                while(ptr!=slow){
+                    ptr=ptr->next;
+                    slow=slow->next;
+                }
+                return ptr;
+            }
+        }
+        return nullptr;
+    }
+};

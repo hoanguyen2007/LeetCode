a843c2fabe60b177fb0f84a661ace15dea8a7777
hoanguyen2007 <118914953+hoanguyen2007@users.noreply.github.com>
2026-07-17 11:07:41 +0700

    File updated at 7/17/2026, 11:07:43 AM

--- 382-LinkedListRandomNode/382-LinkedListRandomNode.cpp
+++ 382-LinkedListRandomNode/382-LinkedListRandomNode.cpp
@@ -1,39 +1,44 @@
-// Last updated: 7/17/2026, 10:51:34 AM
-1/**
-2 * Definition for singly-linked list.
-3 * struct ListNode {
-4 *     int val;
-5 *     ListNode *next;
-6 *     ListNode() : val(0), next(nullptr) {}
-7 *     ListNode(int x) : val(x), next(nullptr) {}
-8 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
-9 * };
-10 */
-11class Solution {
-12private:
-13    ListNode* headNode;
-14public:
-15    Solution(ListNode* head) {
-16        headNode = head;
-17    }
-18    
-19    int getRandom() {
-20        ListNode* current = headNode;
-21        int result = 0;
-22        int i = 1;
-23        while (current!=nullptr){
-24            if (rand() % i == 0) {
-25                result = current->val;
-26            }
-27            i++;
-28            current = current->next;
-29        }
-30        return result;
-31    }
-32};
-33
-34/**
-35 * Your Solution object will be instantiated and called as such:
-36 * Solution* obj = new Solution(head);
-37 * int param_1 = obj->getRandom();
-38 */
+// Last updated: 7/17/2026, 11:07:43 AM
+1
+2/**
+3 * Definition for singly-linked list.
+4 * struct ListNode {
+5 *     int val;
+6 *     ListNode *next;
+7 *     ListNode() : val(0), next(nullptr) {}
+8 *     ListNode(int x) : val(x), next(nullptr) {}
+9 *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+10 * };
+11 */
+12class Solution {
+13public:
+14    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+15        stack <int> s1,s2;
+16        while (l1!=nullptr){
+17            s1.push(l1->val);
+18            l1 = l1->next;
+19        }
+20        while (l2!=nullptr){
+21            s2.push(l2->val);
+22            l2 = l2->next;
+23        }
+24        int carry = 0;
+25        ListNode* head = nullptr;
+26        while (!s1.empty() || !s2.empty() || carry>0){
+27            int sum = carry;
+28            if (!s1.empty()) {
+29                sum += s1.top();
+30                s1.pop();
+31            }
+32            if (!s2.empty()) {
+33                sum += s2.top();
+34                s2.pop();
+35            }
+36            carry = sum / 10;
+37            ListNode* newNode = new ListNode(sum % 10);
+38            newNode->next = head;
+39            head = newNode;
+40        } 
+41        return head;
+42    }
+43};
